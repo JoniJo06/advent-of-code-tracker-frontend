4b715ef22089674e479343b47ce163e83cc38fb2
jonijo06 <jonasjuergens6@gmail.com>
2022-02-28 10:16:26 +0100

    add localstorage entry to keep the actual year open after page refresh

--- src/Pages/BasicTracker/BasicTracker.tsx
+++ src/Pages/BasicTracker/BasicTracker.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Wrapper } from './BasicTracker.styles';
 import { Accordion, AccordionDetails, AccordionSummary, Box, Checkbox, Divider, Typography } from '@mui/material';
-import { CheckBox as CheckBoxIcon, ExpandMore as ExpandMoreIcon } from '@mui/icons-material';
-import yearsInfo from '../../daysInfo.json'
+import {
+  CheckBox as CheckBoxIcon,
+  CheckBoxOutlineBlank as CheckBoxOutlineBlankIcon,
+  ExpandMore as ExpandMoreIcon,
+} from '@mui/icons-material';
+import yearsInfo from '../../daysInfo.json';
 
 type YearStatusType = Record<number, boolean[]>
 
@@ -15,21 +19,25 @@ const BasicTracker = () => {
   const handleChange =
           (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
             setExpanded(isExpanded ? panel : false);
+            localStorage.setItem('openYear', JSON.stringify(isExpanded ? panel : false))
           };
 
   useEffect(() => {
     //@ts-ignore
     setYearsStatus(JSON.parse(localStorage.getItem('yearsInfo')));
+    //@ts-ignore
+    setExpanded(JSON.parse(localStorage.getItem('openYear')))
   }, []);
 
   useEffect(() => {
     if (Object.keys(yearsStatus).length > 0)
       localStorage.setItem('yearsInfo', JSON.stringify(yearsStatus));
+
   }, [ yearsStatus ]);
 
   return (
     <Wrapper>
-      {YEARS.map((year, i) => {
+      {Object.keys(yearsStatus).length > 0 && YEARS.map((year, i) => {
         return (
           <Accordion key={i} expanded={expanded === `panel${i}`} onChange={handleChange(`panel${i}`)}>
             <AccordionSummary
@@ -40,17 +48,20 @@ const BasicTracker = () => {
               </Typography>
               <Typography sx={{ flexGrow: 1 }} />
               <div style={{ display: 'flex', alignItems: 'center' }}>
-                <CheckBoxIcon fontSize='small' color={yearsStatus[year].filter(e => e).length === 25 ? 'primary': 'secondary'}/>
-                {Object.keys(yearsStatus).length > 0 &&
-                 <Typography sx={{ color: 'text.secondary' }}>{yearsStatus[year].filter(e => e).length}/25</Typography>}
+                {yearsStatus[year].filter(e => e).length === 25 ? (
+                  <CheckBoxIcon fontSize='small' color='primary' />
+                ) : (
+                   <CheckBoxOutlineBlankIcon fontSize='small' color='disabled' />)}
+
+                <Typography sx={{ color: 'text.secondary' }}>{yearsStatus[year].filter(e => e).length}/25</Typography>
               </div>
             </AccordionSummary>
             <AccordionDetails sx={{ paddingX: 0 }}>
               {yearsStatus[year]?.map((_, i) => {
                 return <React.Fragment key={i}>
-                  <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+                  <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', paddingLeft: 2 }}>
                     {/*@ts-ignore*/}
-                    <Typography sx={{}}>{`Day ${i+1}: ${yearsInfo[year][i + 1].title}`}</Typography>
+                    <Typography sx={{}}>{`Day ${i + 1}: ${yearsInfo[year][i + 1].title}`}</Typography>
                     <Checkbox
                       checked={yearsStatus[year][i]} onClick={() => {
                       setYearsStatus(prev => {
