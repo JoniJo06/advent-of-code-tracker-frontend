6226ed65f68f0e79a54d74dd628722953a7d643e
JoniJo06 <jonasjuergens6@gmail.com>
2022-04-11 16:11:06 +0200

    add form, submitbutton and language selection

--- src/Pages/CreatePersonalTracker/CreatePersonalTracker.styles.ts
+++ src/Pages/CreatePersonalTracker/CreatePersonalTracker.styles.ts
@@ -2,3 +2,8 @@ import {styled} from '@mui/material/styles';
 import { Container } from '@mui/material';
 
 export const Wrapper = styled(Container)(({theme}) => ({}))
+
+export const Form = styled('form')(({theme})=>({
+  display:'flex',
+  flexDirection: 'column'
+}))
--- src/Pages/CreatePersonalTracker/CreatePersonalTracker.tsx
+++ src/Pages/CreatePersonalTracker/CreatePersonalTracker.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import {Wrapper} from './CreatePersonalTracker.styles'
+import React, { useState } from 'react';
+import {Wrapper, Form} from './CreatePersonalTracker.styles'
 import { Typography } from '@mui/material';
+import {SearchSelectBox} from '../../Components';
+import {Button} from '@mui/material'
 
 const CreatePersonalTracker = () => {
+  const [selectedLanguages, setSelectedLanguages] = useState<string[]>([])
+    const handleClick  = () => {
+    console.log(selectedLanguages)
+  }
+
+  const handleSubmit = (e:React.SyntheticEvent) => {
+    e.preventDefault()
+
+  }
+
   return (
     <Wrapper>
       <Typography variant='h3' align='center'>Create Personal Tracker</Typography>
+      <Form onSubmit={handleSubmit}>
+        <SearchSelectBox setValue={setSelectedLanguages} />
+        <Button sx={{alignSelf: 'center', width: '80%', mt: '20px', fontSize: '1.25rem'}}  size='small' variant='contained' type='submit'>Create</Button>
+      </Form>
     </Wrapper>
   )
 }
